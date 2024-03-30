94cb2298e8d002cb705f7bd606a684f618613f0f
Wei Ji <weiji1994@hotmail.com>
2024-03-30 16:44:31 +1300

    :children_crossing: Set header link anchors
    Have proper 'permalink' anchors for each of the headings on my website! This has been on my todo list for the longest time, as navigating from the links in my CV page to sections in other pages (in community/academic/professional) has been broken for ages. Need to remote the date part from the academic permalinks, decamelize the slugs, and set the permalink style to header link.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,27 @@
 const activityPubPlugin = require('eleventy-plugin-activity-pub');
 const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const slugify = require('@sindresorhus/slugify');
 
 module.exports = function (eleventyConfig) {
+  // Options for the `markdown-it` library
+  const markdownItOptions = {
+    html: true,
+  }
+  // Options for the `markdown-it-anchor` library
+  const markdownItAnchorOptions = {
+    permalink: markdownItAnchor.permalink.headerLink({ safariReaderFix: true }),
+    slugify: s => slugify(s, {decamelize: false})
+  }
+  //
+  const markdownLib = markdownIt(markdownItOptions).use(
+    markdownItAnchor,
+    markdownItAnchorOptions
+  )
+  eleventyConfig.setLibrary("md", markdownLib)
+
   // Add plugins
   eleventyConfig.addPlugin(activityPubPlugin, {
     domain: 'weiji14.github.io',
